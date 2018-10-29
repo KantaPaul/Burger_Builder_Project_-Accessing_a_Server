cef2ae27b9ff840cf007b9b41016403b4d00de6e
KantaPaul <pobon.paul1994@gmai.com>
2018-10-30 01:40:42 +0600

    this section all done

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
+import classes from '../../assets/css/style.scss'
+
+
+let burger = (props) => {
+  let transformIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+    return [...Array(props.ingredients[ingredientKey])].map((__, i) => {
+      return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+    });
+  }).reduce((arr, el) => {
+    return arr.concat(el);
+  }, [])
+
+  return (
+    <div className={classes.burger}>
+      <BurgerIngredient type="bread-top"/>
+      {transformIngredients.length === 0 ? 'Please Update Ingredient' : transformIngredients}
+      <BurgerIngredient type="bread-bottom"/>
+    </div>
+  )
+}
+
+export default burger;
